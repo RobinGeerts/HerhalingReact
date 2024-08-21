1a69dfad300ef179afd25e27e9d812c3e5fd0c83
RobinGeerts <papichulio1@hotmail.com>
2024-08-21 20:13:52 +0200

    commit Api connectie problemen orgineel

--- src/api/motorAPI.ts
+++ src/api/motorAPI.ts
@@ -5,17 +5,17 @@ import {useQuery, UseQueryResult} from '@tanstack/react-query'
 const API_KEY = import.meta.env.VITE_Motorcycle
 
 const client = axios.create({
-    baseURL: 'https://api.api-ninjas.com/v1/motorcycles?data'
+    baseURL: 'https://api.api-ninjas.com/v1/motorcycles/data'
 })
 
-export const useGetMotor = (make: string, model : string): UseQueryResult<IMotor[]> => {
-    return useQuery(['make', make], () => getMotor(make, model), {
+export const useGetMotor = (make: string, model: string): UseQueryResult<IMotor[]> => {
+    return useQuery(['motor', make], () => getMotor(make, model), {
         refetchInterval: 5 * 60 * 1000,
     })
 }
 
 const getMotor = async (make: string, model: string): Promise<IMotor[]> => {
-    const {data} = await client.get<'https://api.api-ninjas.com/v1/motorcycles/data'>(`/`, {
+    const {data} = await client.get<IMotor[]>(`/`, {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${API_KEY}`
